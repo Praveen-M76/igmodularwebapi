c16443f8b79ece86a45d6833bcaa6faaa43ead3a
Praveen-M76 <praveenmathu20@gmail.com>
2026-03-05 10:45:51 +0530

    Update Program.cs

--- ModularPluginWebApi/Program.cs
+++ ModularPluginWebApi/Program.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Controllers
 builder.Services.AddControllers();
 
+// Plugin folder path
 var pluginPath = Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
 
 if (Directory.Exists(pluginPath))
@@ -12,19 +14,27 @@ if (Directory.Exists(pluginPath))
     foreach (var file in Directory.GetFiles(pluginPath, "*.dll"))
     {
         var assembly = Assembly.LoadFrom(file);
+
         builder.Services.AddControllers()
             .PartManager.ApplicationParts.Add(new AssemblyPart(assembly));
     }
 }
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Enable Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Root URL redirect to Swagger
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
+// Map Controllers
 app.MapControllers();
 
+// Run App
 app.Run();
