c19e70682c754d3857354c112cd58a76756c7ee4
praveen M <praveenmathu20@gmail.com>
2026-03-24 17:20:27 +0530

    add swagger basic auth on root

--- ModularPluginWebApi/SwaggerBasicAuthMiddleware.cs
+++ ModularPluginWebApi/SwaggerBasicAuthMiddleware.cs
@@ -11,29 +11,35 @@ public class SwaggerBasicAuthMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
-        if (context.Request.Path.StartsWithSegments("/swagger"))
+        if (context.Request.Path.StartsWithSegments("/swagger") ||
+            context.Request.Path == "/" ||
+            context.Request.Path == "/index.html")
         {
             string authHeader = context.Request.Headers["Authorization"];
 
-            if (authHeader != null && authHeader.StartsWith("Basic "))
+            if (!string.IsNullOrEmpty(authHeader) && authHeader.StartsWith("Basic "))
             {
                 var encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
                 var encoding = Encoding.GetEncoding("iso-8859-1");
                 var usernamePassword = encoding.GetString(Convert.FromBase64String(encodedUsernamePassword));
 
                 var parts = usernamePassword.Split(':');
-                var username = parts[0];
-                var password = parts[1];
-
-                if (username == "admin" && password == "1234")
+                if (parts.Length == 2)
                 {
-                    await _next(context);
-                    return;
+                    var username = parts[0];
+                    var password = parts[1];
+
+                    if (username == "admin" && password == "1234")
+                    {
+                        await _next(context);
+                        return;
+                    }
                 }
             }
 
             context.Response.Headers["WWW-Authenticate"] = "Basic";
             context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Authentication required.");
             return;
         }
 
