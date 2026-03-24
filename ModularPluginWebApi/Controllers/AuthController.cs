96f64dba886da3d2b3c42c9d018e6a759788cc07
praveen M <praveenmathu20@gmail.com>
2026-03-24 12:40:10 +0530

    added login api

---
+++ ModularPluginWebApi/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModularPluginWebApi.Models;
+
+namespace ModularPluginWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            if (model.Username == "admin" && model.Password == "1234")
+            {
+                return Ok(new { message = "Login Successful" });
+            }
+
+            return Unauthorized(new { message = "Invalid Username or Password" });
+        }
+    }
+}
---
+++ ModularPluginWebApi/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ModularPluginWebApi.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
