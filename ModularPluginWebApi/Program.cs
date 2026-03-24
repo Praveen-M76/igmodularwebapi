bcbc49f7e55fe860b18de082132f30bac198b575
praveen M <praveenmathu20@gmail.com>
2026-03-24 18:02:36 +0530

    swagger index fix

--- ModularPluginWebApi/Program.cs
+++ ModularPluginWebApi/Program.cs
@@ -93,11 +93,13 @@ var app = builder.Build();
 
 // Swagger
 app.UseSwagger();
+
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserPluginAPI v1");
-    c.RoutePrefix = "index";   // URL => /index.html
-    c.EnableDeepLinking();
+
+    // This creates /index.html
+    c.RoutePrefix = "index";
 });
 
 // redirect root -> /index.html
