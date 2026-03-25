f8a97e20c9b785a1d1520ad77e0a091f448350b4
praveen M <praveenmathu20@gmail.com>
2026-03-25 15:22:56 +0530

    add jwt login and protected users api

--- ModularPluginWebApi/Controllers/LoginController.cs
+++ ModularPluginWebApi/Controllers/LoginController.cs
@@ -1,55 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace ModularPluginWebApi.Controllers
 {
-    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
     [Route("api/[controller]")]
+    [ApiController]
     public class LoginController : ControllerBase
     {
-        private readonly IConfiguration _configuration;
-
-        public LoginController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        [HttpPost("login")]
-        public IActionResult Login(string username, string password)
-        {
-            if (username == "admin" && password == "1234")
-            {
-                var claims = new[]
-                {
-                    new Claim(ClaimTypes.Name, username),
-                    new Claim(ClaimTypes.Role, "Admin")
-                };
-
-                var key = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!)
-                );
-
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-                var token = new JwtSecurityToken(
-                    claims: claims,
-                    expires: DateTime.UtcNow.AddHours(1),
-                    signingCredentials: creds
-                );
-
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-
-                return Ok(new
-                {
-                    message = "Login Success",
-                    token = tokenString
-                });
-            }
-
-            return Unauthorized(new { message = "Invalid username or password" });
-        }
     }
 }
